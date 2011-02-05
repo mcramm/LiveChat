3ae1c54e5c8f7e3e79d633609b087250580c9b72
mcramm <GMCramm@gmail.com>
2011-02-04 22:38:15 -0700

    Removing pointless requires

--- environment.rb
+++ environment.rb
@@ -2,8 +2,6 @@ require 'compass'
 require 'haml'
 require 'couchrest'
 require 'couchrest_extended_document'
-require 'ostruct'
-require 'cgi'
 require 'lib/user'
 
 ENV['RACK_ENV'] ||= 'production'
